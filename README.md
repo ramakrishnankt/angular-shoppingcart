f9bb615fc84bbffdd970409b99fc857231dd9d0e
ramakrishnankt <rettaikulamram@gmail.com>
2020-03-01 02:06:46 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-iv74rd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-iv74rd)
---
+++ src/app/shipping/shipping.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import{CartService} from '../cart.service';
+
+@Component({
+  selector: 'app-shipping',
+  templateUrl: './shipping.component.html',
+  styleUrls: ['./shipping.component.css']
+})
+export class ShippingComponent implements OnInit {
+shippingCosts;
+  constructor(private cartservice:CartService) { }
+
+  ngOnInit() {
+    this.shippingCosts=this.cartservice.getShippingPrices();
+  }
+
+}
